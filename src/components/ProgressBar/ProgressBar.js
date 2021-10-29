3ea6d95d39e81328e85358d6815fe1476cadda69
Tess Huang <tesshuangxj@yahoo.com>
2021-10-28 21:00:04 -0700

    Update styling for progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,46 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZES = {
+  small: {
+    '--height': '8px'
+  },
+  medium: {
+    '--height': '12px'
+  },
+  large: {
+    '--height': '24px'
+  },
 };
+
+const ProgressBar = ({ value, size }) => {
+  return( 
+    <Wrapper>
+      <Inner 
+        role="progressbar"
+        aria-valuenow={value}
+        aria-valuemin="0"
+        aria-valuemax="100"
+        value={value}
+        style={SIZES[size]}
+        />
+      <VisuallyHidden>The progressbar value is {value}%</VisuallyHidden>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  background: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 8px;
+  padding: 4px;
+`;
+
+const Inner = styled.div`
+  background: ${COLORS.primary};
+  height: var(--height);
+  width: ${p =>  p.value > 100 ? '100%' : `${p.value}%`};
+  border-radius: ${p =>  p.value >= 100? '4px' : '4px 0px 0px 4px'} ;
+`;
 
 export default ProgressBar;
