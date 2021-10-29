cd344b6e8139c15609f6662e381e7882a510a1a3
Tess Huang <tesshuangxj@yahoo.com>
2021-10-28 21:30:36 -0700

    Update html structure for progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -23,15 +23,18 @@ const SIZES = {
 const ProgressBar = ({ value, size }) => {
   return( 
     <Wrapper
-      style={{ '--padding': `${SIZES[size].padding}px`}}
-      >
-      <Inner 
-        role="progressbar"
-        aria-valuenow={value}
-        aria-valuemin="0"
-        aria-valuemax="100"
-        style={{ '--height': `${SIZES[size].height}px`, '--width': `${value}%`}}
-        />
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{ '--padding': `${SIZES[size].padding}px`}}>
+        <InnerWrapper>
+          <Inner 
+            style={{ 
+              '--height': `${SIZES[size].height}px`, 
+              '--width': `${value}%`
+            }}/>
+        </InnerWrapper>
       <VisuallyHidden>The progressbar value is {value}%</VisuallyHidden>
     </Wrapper>
   );
@@ -40,16 +43,20 @@ const ProgressBar = ({ value, size }) => {
 const Wrapper = styled.div`
   background: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--padding);
+  border-radius: 8px;
+`;
+
+const InnerWrapper = styled.div`
   border-radius: 8px;
   overflow: hidden;
-  padding: var(--padding);
 `;
 
 const Inner = styled.div`
   background: ${COLORS.primary};
   height: var(--height);
   width: var(--width);
-  border-radius: ${p =>  p.value >= 100? '4px' : '4px 0px 0px 4px'} ;
+  border-radius: '4px 0px 0px 4px';
 `;
 
 export default ProgressBar;
